357e3d16d00aee9fda4cbdee0d2bc6f9a1eeb133
Patrick Downey <patrickdowney@dioadconsulting.com>
2019-09-10 22:09:27 +0100

    Update module reference to use correct remote location

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
 module strict_headers {
-  source = "git::https://github.com/dioadconsulting/aws-cloudfront-https-strict-headers-lambda?ref=0.1.0"
+  source = "git::https://github.com/dioadconsulting/terraform-aws-cloudfront-https-strict-headers-lambda?ref=0.1.0"
 
   suffix = "${var.domain_name}"
 
