a30cb8ab4dd97cb8fd713965577cd68ca665389f
Patrick Downey <patrickdowney@dioadconsulting.com>
2020-06-27 20:13:56 +0100

    tf 0.12 syntax updates

--- acm.tf
+++ acm.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cert" {
-  provider = "aws.us-east-1"
+  provider = aws.us-east-1
 
   domain_name       = var.domain_name
   validation_method = "DNS"
@@ -12,11 +12,11 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  provider = "aws.us-east-1"
-  name     = "${aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_name}"
-  type     = "${aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_type}"
+  provider = aws.us-east-1
+  name     = aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_name
+  type     = aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_type
   zone_id  = var.hosted_zone_id
-  records  = ["${aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_value}"]
+  records  = [aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_value]
   ttl      = 60
 
   count = length(var.domain_aliases)
@@ -24,18 +24,18 @@ resource "aws_route53_record" "cert_validation" {
 
 
 resource "aws_route53_record" "cert_validation_0" {
-  provider = "aws.us-east-1"
-  name     = "${aws_acm_certificate.cert.domain_validation_options[0].resource_record_name}"
-  type     = "${aws_acm_certificate.cert.domain_validation_options[0].resource_record_type}"
+  provider = aws.us-east-1
+  name     = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+  type     = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
   zone_id  = var.hosted_zone_id
-  records  = ["${aws_acm_certificate.cert.domain_validation_options[0].resource_record_value}"]
+  records  = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
   ttl      = 60
 
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  provider = "aws.us-east-1"
+  provider = aws.us-east-1
 
-  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = concat([aws_route53_record.cert_validation_0.fqdn], aws_route53_record.cert_validation[*].fqdn)
 }
