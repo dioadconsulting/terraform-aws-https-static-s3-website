4130f0f5da1c2ace3680856d41e1e5d6c6834ae8
Patrick Downey <patrickdowney@dioadconsulting.com>
2020-06-27 20:34:29 +0100

    Update lamda terraform module version

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
 module strict_headers {
-  source = "git::https://github.com/dioadconsulting/terraform-aws-cloudfront-https-strict-headers-lambda?ref=0.1.1"
+  source = "git::https://github.com/dioadconsulting/terraform-aws-cloudfront-https-strict-headers-lambda?ref=0.1.2"
 
   suffix = var.domain_name
 
