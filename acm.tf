656871dd7a96852eab8a73c92471de1530d2cd99
Patrick Downey <patrickdowney@dioadconsulting.com>
2021-04-06 21:36:56 +0100

    update acm syntax

--- acm.tf
+++ acm.tf
@@ -12,30 +12,27 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  provider = aws.us-east-1
-  name     = aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_name
-  type     = aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_type
-  zone_id  = var.hosted_zone_id
-  records  = [aws_acm_certificate.cert.domain_validation_options[1 + count.index].resource_record_value]
-  ttl      = 60
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-  count = length(var.domain_aliases)
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.hosted_zone_id
 }
 
 
-resource "aws_route53_record" "cert_validation_0" {
-  provider = aws.us-east-1
-  name     = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type     = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
-  zone_id  = var.hosted_zone_id
-  records  = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
-  ttl      = 60
-
-}
 
 resource "aws_acm_certificate_validation" "cert" {
   provider = aws.us-east-1
 
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = concat([aws_route53_record.cert_validation_0.fqdn], aws_route53_record.cert_validation[*].fqdn)
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
